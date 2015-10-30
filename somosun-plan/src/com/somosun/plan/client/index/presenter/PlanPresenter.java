830b74aac4dcd439bc3e96058c6cd3dda93024ec
Mike Woodcock <mwc1.0@hotmail.com>
2015-10-30 12:30:42 -0500

    Completing decouments

--- somosun-plan/src/com/somosun/plan/client/index/presenter/PlanPresenter.java
+++ somosun-plan/src/com/somosun/plan/client/index/presenter/PlanPresenter.java
@@ -1940,7 +1940,8 @@ ComplementaryValueView.Presenter{
 	}
 	
 	/**
-	 * This method will take care into account the number of subjects in the semester to select the semi-final value for the variable. It will add the subjects to the plan too.
+	 * This method will take care into account the number of subjects in the semester to select the semi-final value for the variable. It will add the subjects to the plan too. 
+	 * This method will also check it the subject is a default subject in order to check it that default subjects has been fulfilled 
 	 * 
 	 * @param s
 	 * @param cV
