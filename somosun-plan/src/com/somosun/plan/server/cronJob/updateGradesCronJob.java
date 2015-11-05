726e32c725669056d15a4efd0636a12c91955318
Mike Woodcock <mwc1.0@hotmail.com>
2015-11-05 08:11:35 -0500

    Adding a test http servlet in order to get a cron job

---
+++ somosun-plan/src/com/somosun/plan/server/cronJob/updateGradesCronJob.java
@@ -0,0 +1,20 @@
+package com.somosun.plan.server.cronJob;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class updateGradesCronJob extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("this the doGet of a http servelt");
+	}
+
+}
