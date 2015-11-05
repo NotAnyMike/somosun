8f73f61c00e013a91f2fb621c54081e528748af4
Mike Woodcock <mwc1.0@hotmail.com>
2015-11-05 08:44:24 -0500

    Added the cron job and working, closes #68

--- somosun-plan/src/com/somosun/plan/server/cronJob/updateGradesCronJob.java
+++ somosun-plan/src/com/somosun/plan/server/cronJob/updateGradesCronJob.java
@@ -2,6 +2,7 @@ package com.somosun.plan.server.cronJob;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 
 public class updateGradesCronJob extends HttpServlet {
 
+	private static final Logger log = Logger.getLogger("SunServiceImpl");
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
+		log.info("this the doGet of a http servelt");
 		out.println("this the doGet of a http servelt");
 	}
 
